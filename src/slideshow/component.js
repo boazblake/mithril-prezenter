08e82548a2b5ad132a056e9e1f09cf70269309d2
bblake <bblake@goempyrean.com>
2019-04-16 18:54:58 -0500

    some updates

--- src/slideshow/component.js
+++ src/slideshow/component.js
@@ -1,40 +1,45 @@
-import m from 'mithril'
-import { pluck } from 'ramda'
-import Remarkable from 'remarkable'
+import m from "mithril"
+import { pluck } from "ramda"
+import Remarkable from "remarkable"
+import { animateEntrance } from "../services/animations.js"
 
-const md = new Remarkable('full', {
+const md = new Remarkable("full", {
   html: false,
   xhtmlOut: false,
   breaks: false,
-  langPrefix: 'language-',
+  langPrefix: "language-",
   linkify: true,
-  linkTarget: '',
+  linkTarget: "",
   typographer: true,
-  quotes: '“”‘’',
-});
-
-
+  quotes: "“”‘’"
+})
 
 const SlideShow = ({ attrs: { Models } }) => {
   const state = {
     cursor: 0,
-    isFullscreenWidth: 'vh',
-    isFullscreenHeight: '%',
+    isFullscreenWidth: "vh",
+    isFullscreenHeight: "%",
     clicks: 0,
     size: Models.CurrentPresentation.slideShow().length,
-    contents: pluck('content', Models.CurrentPresentation.slideShow()),
+    contents: pluck("content", Models.CurrentPresentation.slideShow())
   }
 
-  const nextSlide = () => (state.cursor == state.size ? '' : state.cursor++)
+  const nextSlide = () => {
+    document.body.scrollTop = 0
+    state.cursor == state.size ? "" : state.cursor++
+  }
 
-  const prevSlide = () => (state.cursor == 0 ? '' : state.cursor--)
+  const prevSlide = () => {
+    document.body.scrollTop = 0
+    state.cursor == 0 ? "" : state.cursor--
+  }
 
-  const changeSlide = key => {
+  const changeSlide = (key) => {
     switch (key) {
-      case 'ArrowLeft':
+      case "ArrowLeft":
         prevSlide()
         break
-      case 'ArrowRight':
+      case "ArrowRight":
         nextSlide()
         break
     }
@@ -44,21 +49,22 @@ const SlideShow = ({ attrs: { Models } }) => {
     oninit: (state.slide = state.contents[state.cursor]),
     view: ({ attrs: { Models } }) => {
       return m(
-        '.slideshow',
+        ".slideshow",
         {
           tabindex: 0,
           onkeyup: ({ key }) => {
             changeSlide(key)
-          },
+          }
         },
-          m('.slidecard',
-            { onupdate: ({ dom }) => {
-              dom.style.animation = "stretchLeft 1s ease-in-out"
-              return dom
-            }},
-          m.trust(md.render(state.contents[state.cursor]) || '~ FIN ~'))
+        m(
+          ".slidecard",
+          {
+            onupdate: ({ dom }) => animateEntrance({ dom })
+          },
+          m.trust(md.render(state.contents[state.cursor]) || "~ FIN ~")
+        )
       )
-    },
+    }
   }
 }
 
