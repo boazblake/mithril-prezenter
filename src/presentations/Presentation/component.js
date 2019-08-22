4531b5c2787c233b890036af4ea1a0b292f07471
bblake <bblake@goempyrean.com>
2019-08-22 12:57:25 -0500

    updated styling and fonts

--- src/presentations/Presentation/component.js
+++ src/presentations/Presentation/component.js
@@ -5,39 +5,37 @@ import { deletePresentationsTask } from "../model.js"
 import Task from "data.task"
 
 const Presentation = ({ attrs: { title, id, Models } }) => {
-  const onError = task => error => log(`error with ${task}`)(error)
-  const onSuccess = models => deleted => {
+  const onError = (task) => (error) => log(`error with ${task}`)(error)
+  const onSuccess = (models) => (deleted) => {
     return (Models.Presentations = without([deleted], Models.Presentations))
   }
 
-  const authDeleteTask = id =>
+  const authDeleteTask = (id) =>
     window.confirm("Are you sure you want to delete?")
       ? Task.of(id)
       : Task.rejected(id)
 
-  const removePresTask = pId =>
+  const removePresTask = (pId) =>
     authDeleteTask(pId)
       .chain(deletePresentationsTask)
       .fork(onError("deleting"), onSuccess(Models))
 
   return {
     view: ({ attrs: { title, id, Models } }) =>
-      m(".card", [
-        m("div.card-header", [
-          m(
-            ".card-btn.full-width",
-            {
-              onclick: () => m.route.set(`/presentation/${id}/slides`),
-            },
-            title
-          ),
-          m("button.card-delete", {
-            onclick: () => removePresTask(id),
-          }),
-        ]),
-        m("div.card-body", []),
-        m("div.card-footer", []),
-      ]),
+      m(
+        ".card",
+        {
+          onclick: () => m.route.set(`/presentation/${id}/slides`)
+        },
+        [
+          m("div.card-header.card-btn", [
+            title,
+            m("button.card-delete", {
+              onclick: () => removePresTask(id)
+            })
+          ])
+        ]
+      )
   }
 }
 
